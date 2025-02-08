f08fc9afa2c6ea0e42cf2f5fc877af8dc884d366
athul <athuli057@gmail.com>
2025-02-08 19:09:51 +0530

    first commit

---
+++ graph/sample.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("../usr/input.txt", "r", stdin);
+	freopen("../usr/output.txt", "w", stdout);
+
+#endif
+
+	ll t;
+	cin >> t;
+	cout << "your number is" << t;
+}
